4e12d83e74f442caffaec5b0d2651178bc614638
SamSammm78 <samuel.thepekka77pgmail.com>
2025-04-20 14:52:07 +0200

    website version

--- Website Version/script.js
+++ Website Version/script.js
@@ -1,5 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const link = urlParams.get('url');
+
+var showUrl = document.getElementById('ItemLink')
+showUrl.innerHTML = "Item Link : " + link;
+
 document.getElementById('dataForm').addEventListener('submit', function(event) {
-    var currentUrl = document.getElementById('ItemLink').value;
     var id = ""
     var itemName = document.getElementById('ItemName').value;
     var image = document.getElementById('Image').value;
@@ -9,7 +14,7 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
     var url = "https://script.google.com/macros/s/AKfycbzRf9Uc6QaSC-yGuO29S8s71mkim6LZs28feA_qIVtFBhdT3WYvq2vE1mEeFz9zZsl5Aw/exec"
 
     // Créer l'URL avec les paramètres
-    var requestUrl = `${url}?ID=${encodeURIComponent(id)}&Item%20Name=${encodeURIComponent(itemName)}&Image=${encodeURIComponent(image)}&Links=${encodeURIComponent(currentUrl)}&Price=${encodeURIComponent(price)}&Type=${encodeURIComponent(type)}`;
+    var requestUrl = `${url}?ID=${encodeURIComponent(id)}&Item%20Name=${encodeURIComponent(itemName)}&Image=${encodeURIComponent(image)}&Links=${encodeURIComponent(link)}&Price=${encodeURIComponent(price)}&Type=${encodeURIComponent(type)}`;
     
     // Envoyer la requête GET
     fetch(requestUrl)
